97d91b0b334f9ee092f64a471284cd9b38c41bd8
Timmy Grant <timmylibblya@gmail.com>
2020-12-02 09:48:57 -0500

    Webpage matches using DOM to manipulate

--- js/index.js
+++ js/index.js
@@ -40,3 +40,91 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = "img/header-img.png";
+
+const title = document.querySelector('h1');
+title.textContent = 'DOM Is Awesome';
+
+const buttonOne = document.querySelector('button');
+buttonOne.textContent = 'Get Started';
+
+const serviceLink = document.createElement("a");
+serviceLink.textContent = "Services";
+serviceLink.href = "#";
+document.querySelector("nav").appendChild(serviceLink);
+
+const productLink = document.createElement("a");
+productLink.textContent = "Product";
+productLink.href = "#";
+document.querySelector("nav").appendChild(productLink);
+
+const visionLink = document.createElement("a");
+visionLink.textContent = "Vision";
+visionLink.href = "#";
+document.querySelector("nav").appendChild(visionLink);
+
+const featureLink = document.createElement("a");
+featureLink.textContent = "Features";
+featureLink.href = "#";
+document.querySelector("nav").appendChild(featureLink);
+
+const aboutLink = document.createElement("a");
+aboutLink.textContent = "About";
+aboutLink.href = "#";
+document.querySelector("nav").appendChild(aboutLink);
+
+const contactLink = document.createElement("a");
+contactLink.textContent = "Contact";
+contactLink.href = "#";
+document.querySelector("nav").appendChild(contactLink);
+
+const featureSec = document.querySelector('.text-content:nth-of-type(1)');
+const featTitle = featureSec.querySelector('h4');
+const featContent = featureSec.querySelector('p');
+featTitle.textContent = 'Features';
+featContent.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const aboutSec = document.querySelector('.text-content:nth-of-type(2)');
+const aboutTitle = aboutSec.querySelector('h4');
+const aboutContent = aboutSec.querySelector('p');
+aboutTitle.textContent = 'About';
+aboutContent.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const midImg = document.querySelector('.middle-img');
+midImg.src = "img/mid-page-accent.jpg";
+
+const bottomSec = document.querySelector('.bottom-content')
+
+const serviceSec = bottomSec.querySelector('.text-content:nth-of-type(1)');
+const serviceTitle = serviceSec.querySelector('h4');
+const serviceContent = serviceSec.querySelector('p');
+serviceTitle.textContent = 'Services';
+serviceContent.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const productSec = bottomSec.querySelector('.text-content:nth-of-type(2)');
+const productTitle = productSec.querySelector('h4');
+const productContent = productSec.querySelector('p');
+productTitle.textContent = 'Product';
+productContent.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const visionSec = bottomSec.querySelector('.text-content:nth-of-type(3)');
+const visionTitle = visionSec.querySelector('h4');
+const visionContent = visionSec.querySelector('p');
+visionTitle.textContent = 'Vision';
+visionContent.textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const contactSec = document.querySelector('.contact');
+const contactTitle = contactSec.querySelector('h4');
+const contactAddress = contactSec.querySelector('p:nth-of-type(1)');
+const contactPhone = contactSec.querySelector('p:nth-of-type(2)');
+const contactEmail = contactSec.querySelector('p:nth-of-type(3)');
+contactTitle.textContent = 'Contact';
+contactAddress.textContent = '123 Way 456 Street Somewhere, USA';
+contactPhone.textContent = '1 (888) 888-8888';
+contactEmail.textContent = 'sales@greatidea.io';
+
+const footSec = document.querySelector('footer');
+const copyrightSec = footSec.querySelector('p');
+copyrightSec.textContent = 'Copyright Great Idea! 2018';
